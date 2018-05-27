6fe9314c446d13cba59f55598107d9ebf13a2e89
Stanislav Popov <popstas@gmail.com>
2018-05-27 05:07:19 +0500

    feat: add icons and fixed chords to SongItem

--- components/SongItem.vue
+++ components/SongItem.vue
@@ -1,15 +1,23 @@
 <template>
   <el-collapse-item :title="title" :name="song.url">
-    <div class="text item">
+    <template slot="title">
+      {{ title }}
+      <i v-if="song.text" class="el-icon-tickets"></i>
+      <i v-if="song.details.chords" class="el-icon-check"></i>
+    </template>
+    <div class="text item chords">
       {{ song.details.chords }}
     </div>
-    <pre>{{ song.html }}</pre>
+    <pre>{{ song.text }}</pre>
     <a target="_blank" :href="song.url">link</a>
   </el-collapse-item>
 </template>
 
 <style>
 .el-collapse-item{ overflow: hidden; }
+.chords{
+  position: fixed; top: 0; left: 0; right: 0;
+  text-align: center; background: #fff; padding: 10px; }
 </style>
 
 <script>
@@ -17,11 +25,12 @@ export default {
   props: ['song'],
   computed: {
     title(){
+      let title = this.song.title
       if(this.song.details.title){
-        return this.song.details.artist + ' - ' + this.song.details.title
-      } else {
-        return this.song.title
+        title = this.song.details.artist + ' - ' + this.song.details.title
       }
+      title = title.trim(',');
+      return title
     }
   }
 }
